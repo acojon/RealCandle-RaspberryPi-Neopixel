6e29cd45b9c6f7d5ab926fe82f6c0db87305453d
jon <acojon@gmail.com>
2019-03-31 08:24:23 -0600

    Add some more comments

--- candle.py
+++ candle.py
@@ -33,6 +33,9 @@ red = 255
 green = 127
 blue = 10
 
+# A steady candle is cool, but a candle that occasionally flickers is cooler.
+# You can set the value between 1 - 10.  The lower the value, the more likely
+# the candle will switch to the flickering pattern.
 flicker_probability = 8
 
 candle = tealight.candle(
@@ -46,10 +49,22 @@ candle = tealight.candle(
 )
 candle.cleanup()
 
+# The candle is playing back the normal/flicker patterns from the .csv files.
+# The CSV files were recorded at 175Hz (2 millisecond intervals.) The patterns
+# don't really look right if you play them back at a slower (or faster)
+# interval.
+#
+# To make things look correct, you need to call the update method every pass
+# through the loop.  Don't add a time.sleep to this loop, it tends to mess up
+# the playback.  The update method internally keeps track of how much time has
+# passed since the neopixel was last updated, and it will update it after 2
+# milliseconds has passed.
 try:
     while True:
         candle.update(chance_flicker = flicker_probability)
 
 except KeyboardInterrupt:	   # When 'Ctrl+C' is pressed,
+
+    # Call the cleanup method to turn the led off
     candle.cleanup()
     exit()
